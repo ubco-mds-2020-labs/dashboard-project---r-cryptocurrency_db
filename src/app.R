68d604dfd1ae358766d73999b44e0e2d5e2a61a1
afreenshakeel24 <afreen.ubc@gmail.com>
2021-03-18 22:51:02 -0700

    Fixed tab5,6

--- src/app.R
+++ src/app.R
@@ -7,13 +7,16 @@ library(readr)
 library(ggplot2)
 library(plotly)
 library(dplyr)
+library(GGally)
 plotly::ggplotly
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
+setwd("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project---r-cryptocurrency_db/")
 #Read data
-structure <- read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project---r-cryptocurrency_db/data/raw_data/structure.csv")
-price <- read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project---r-cryptocurrency_db/data/processed_data/price.csv")
+structure <- read_csv("data/raw_data/structure.csv")
+price <- read_csv("data/processed_data/price.csv")
+#data3 <- read_csv("data/processed_data/data3.csv")
 price_OHLC <- select(price, Open,High,Low,Close,Name,New_date1)
 price_rest <- subset(price, Name != 'bitcoin' & Name != 'bitcoin_cash')
 
@@ -69,7 +72,7 @@ tab1_content =
                     )
                 )
 
-            ), style = list('text-align' = 'center', 'color' = 'cyan', 'background' = 'black')
+            ), style = list('text-align' = 'center', 'color' = '#d4af37')
         ),
 
         # Row about all definations
@@ -82,36 +85,36 @@ tab1_content =
                 ),
                 dbcCol(
                     list(
-                        htmlH6('Opening price on the latest day')
+                        htmlP('Opening price on the latest day')
                     )
                 ),
                 dbcCol(
                     list(
-                        htmlH6('Closing price on the latest day')
+                        htmlP('Closing price on the latest day')
                     )
                 ),
                 dbcCol(
                     list(
-                        htmlH6('Average price of the last 7 days')
+                        htmlP('Average price of the last 7 days')
                     )
                 ),
                 dbcCol(
                     list(
-                        htmlH6('Average price of the last 30 days')
+                        htmlP('Average price of the last 30 days')
                     )
                 ),
                 dbcCol(
                     list(
-                        htmlH6('Market capitalization')
+                        htmlP('Market capitalization')
                     )
                 ),
                 dbcCol(
                     list(
-                        htmlH6('# of transaction on the lastest day')
+                        htmlP('Transaction on the lastest day')
                     )
                 )
 
-            ), style = list('text-align' = 'center', 'color' = 'red', 'background' = 'black')
+            ), style = list('text-align' = 'center', 'color' = 'white')
         ),
 
         # Row about values of  bitcoin
@@ -153,7 +156,7 @@ tab1_content =
                     )
                 )
 
-            ), style = list('text-align' = 'center', 'color' = 'green', 'background' = 'black')
+            ), style = list('text-align' = 'center', 'color' = '#B3D0C9')
         ),
         # Row about values of  rest of the currencies || Need to work on dropdown and respective values
         dbcRow(
@@ -198,7 +201,7 @@ tab1_content =
                     )
                 )
 
-            ), style = list('text-align' = 'center', 'color' = 'pink', 'background' = 'black')
+            ), style = list('text-align' = 'center', 'color' = 'pink')
         ),
         dbcRow(
             list(
@@ -207,7 +210,7 @@ tab1_content =
                         htmlBr()
                     )
                 )
-            ), style = list('background' = 'black')
+            )
         ),
         dbcRow(
             list(
@@ -216,23 +219,24 @@ tab1_content =
                         htmlBr()
                     )
                 )
-            ), style = list('background' = 'black')
+            )
         ),
         dbcRow(
             list(
                 dbcCol(
                     list(
-                        htmlH2("Volume graph")
+                        htmlH2("Volume of all cryptocurrencies"
+                        )
                     )
                 )
-            ), style = list('text-align' = 'center', 'color' = 'purple', 'background' = 'black')
+            ), style = list('text-align' = 'center', 'color' = '#d4af37')
         ),
         dbcRow(
             list(
                 dbcCol(
                     list(
-                        dccGraph(id='vol1')
-                    ), style = list('background' = 'black')
+                        dccGraph(id='vol1',style=list("height"=400))
+                    )
                 )
             )
         ),
@@ -241,7 +245,7 @@ tab1_content =
                 dbcCol(
                     list(
                         #dccGraph(id='time-series1')
-                    ), style = list('background' = 'black')
+                    )
                 )
             )
         )
@@ -268,10 +272,11 @@ tab2_content =
                                            list(label = "Low : Lowest price on the given day", value = "Low"),
                                            list(label = "Close : Closing price on the given day", value = "Close")),
                             value = 'Close'
-                        )
+                        ),
+                        htmlBr()
                     )
                 )
-            ), style = list('text-align' = 'center', 'color' = 'purple', 'background' = 'black')
+            ), style = list('text-align' = 'center', 'color' = 'purple')
         ),
         dbcRow(
             list(
@@ -279,7 +284,7 @@ tab2_content =
                     list(
                         dccGraph(id='group1',
                         style=list("height"=800))
-                    ), style = list('background' = 'black')
+                    )
                 )
             )
         )       
@@ -288,6 +293,36 @@ tab2_content =
 
 
 tab3_content = 
+    dbcContainer(
+        list(
+        dbcRow(
+            list(
+                dbcCol(
+                    list(
+                        htmlH2("Box Plot of all the cryptocurrencies"),
+                        dccGraph(id="quartile1",
+                        style=list("height"=400))
+                    )
+                )
+            )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(htmlBr(),
+                        htmlH2("Market Capitalization of all the cryptocurrencies"),
+                        dccGraph(id="hist1",
+                        style=list("height"=400))
+                )
+            )
+        )
+        )
+    )
+    )
+
+
+
+tab4_content = 
 dbcContainer(
     list(
     dbcRow(
@@ -297,7 +332,7 @@ dbcContainer(
                     htmlBr()
                 )
             )
-        ), style = list('background' = 'black')
+        )
     ),
     dbcRow(
         list(
@@ -306,7 +341,7 @@ dbcContainer(
                     htmlBr()
                 )
             )
-        ), style = list('background' = 'black')
+        )
     ),
     dbcRow(
         list(
@@ -315,7 +350,7 @@ dbcContainer(
                     htmlBr()
                 )
             )
-        ), style = list('background' = 'black')
+        )
     ),
     dbcRow(
         list(
@@ -324,7 +359,7 @@ dbcContainer(
                     htmlBr()
                 )
             )
-        ), style = list('background' = 'black')
+        )
     ),
     dbcRow(
         list(
@@ -333,7 +368,7 @@ dbcContainer(
                     htmlBr()
                 )
             )
-        ), style = list('background' = 'black')
+        )
     ),
     dbcRow(
         list(
@@ -345,14 +380,13 @@ dbcContainer(
                     htmlBr(),
                     htmlH4("Fast and secure way to purchase or exchange cryptocurrencies"),
                     htmlBr(),
-                    htmlH6("Share your email address to subscribe to our weekly updates and investment tips"),
                     htmlBr(),
                     htmlBr(),
                     dbcRow(
                         list(
                             dbcCol(
                                 list(
-                                    dbcInput(placeholder="Type something...", type="text")
+                                    dbcInput(placeholder="Subscribe now...", type="text")
                                 )
                             ),
                             dbcCol(
@@ -370,7 +404,7 @@ dbcContainer(
                     htmlBr(),
                     htmlBr(),
                     htmlBr()
-                )
+                ), style = list('text-align' = 'left')
             ),
             dbcCol(
                 list(
@@ -421,36 +455,143 @@ dbcContainer(
                     htmlDiv(id='crypto1'),
 
                     dbcButton("Buy now", color="light", className="mr-1")
-                )
+                ), style = list('text-align' = 'left')
             )
         )
     )
 )
 )
 
-
-tab4_content = 
-    dbcContainer(
-        list(
+tab5_content = 
+dbcContainer(
+    list(
         dbcRow(
             list(
                 dbcCol(
                     list(
-                        dccGraph(id="quartile1",
-                        style=list("height"=400))
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlH4("Email"),
+                    htmlBr(),
+                    dccInput(type="email", id="email1", placeholder="abc@gmail.com"),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlH4("Password"),
+                    htmlBr(),
+                    dccInput(type="password", id="password1", placeholder="Enter password"),
+                    htmlBr(),
+                    htmlBr(),
+                    dbcButton("Login", color="light", className="mr-1"),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlP("If you are a new user then click below to register"),
+                    dbcButton("Register", color="light", className="mr-1"),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr(),
+                    htmlBr()
+                ))
+            )
+        )
+    )
+)
+
+tab6_content = 
+dbcContainer(
+    list(
+        dbcRow(
+            list(
+                dbcCol(
+                    list(
+                        htmlH2("What is cryptocurrency"),
+                        htmlBr()
+                        )
+                    )
+            )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(
+                        htmlP('Cryptocurrency is digital money. That means there’s no physical coin or bill — it’s all online. You can transfer cryptocurrency to someone online without a go-between, like a bank. Bitcoin and Ether are well-known cryptocurrencies, but new cryptocurrencies continue to be created.'),
+                        htmlBr(),
+                        htmlP('People might use cryptocurrencies for quick payments and to avoid transaction fees. Some might get cryptocurrencies as an investment, hoping the value goes up. You can buy cryptocurrency with a credit card or, in some cases, get it through a process called “mining.” Cryptocurrency is stored in a digital wallet, either online, on your computer, or on other hardware.'),
+                        htmlBr(),
+                        htmlP('Before you buy cryptocurrency, know that it does not have the same protections as when you are using U.S. dollars. Also know that scammers are asking people to pay with cryptocurrency because they know that such payments are typically not reversible.'),
+                        htmlBr(),
+                        htmlBr()
+                    ), style = list('color' = 'white')
+                )#,style = list('color' = 'white')
+            )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(
+                        htmlH2("Cryptocurrencies vs. U.S. Dollars"),
+                        htmlBr()
                     )
                 )
             )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(
+                        htmlP('The fact that cryptocurrencies are digital is not the only important difference between cryptocurrencies and traditional currencies like U.S. dollars.'),
+                        htmlBr()
+                    ), style = list('color' = 'white')
+                )#,style = list('color' = 'white')
+            )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(htmlH4('Cryptocurrencies aren’t backed by a government.'),
+                        htmlBr()
+                    )
+                )
+            )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(
+                        htmlP('Cryptocurrencies are not insured by the government like U.S. bank deposits are. This means that cryptocurrency stored online does not have the same protections as money in a bank account. If you store your cryptocurrency in a digital wallet provided by a company, and the company goes out of business or is hacked, the government may not be able to step and help get your money back as it would with money stored in banks or credit unions.'),
+                        htmlBr()
+                    ), style = list('color' = 'white')
+                )#,style = list('color' = 'white')
+            )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(htmlH4('A cryptocurrency’s value changes constantly.'),
+                        htmlBr()
+                    )
+                )
+            )
+        ),
+        dbcRow(
+            list(
+                dbcCol(
+                    list(
+                        htmlP('A cryptocurrency’s value can change by the hour. An investment that may be worth thousands of U.S. dollars today might be worth only hundreds tomorrow. If the value goes down, there’s no guarantee that it will go up again.'),
+                        htmlBr()
+                    ), style = list('color' = 'white')
+                )
+            )
         )
-        )
-    )
-
-
-
-
-
-        
-
+    ),style = list('color' = '#d4af37', 'background' = "#1C232F", 'text-align' = 'left')
+)
 
 
 
@@ -464,7 +605,12 @@ tabs = htmlDiv(
     htmlH2("Cryptocurrency Reporting Dashboard"),
     dbcTabs(
       list(
-        dbcTab(children=list(
+       dbcTab(children=list(
+          htmlBr(),
+          tab6_content
+        ), 
+        label="About Cryptocurrency"
+       ),dbcTab(children=list(
           htmlBr(),
           tab1_content
         ), 
@@ -480,17 +626,23 @@ tabs = htmlDiv(
           htmlBr(),
           tab3_content
         ), 
-        label="Buy & Sell"
+        label="Summary"
        ),
        dbcTab(children=list(
           htmlBr(),
           tab4_content
         ), 
-        label="Sample"
+        label="Buy & Sell"
+       ),
+       dbcTab(children=list(
+          htmlBr(),
+          tab5_content
+        ), 
+        label="My Account"
        )
       )
     )
-  ),style = list('color' = 'yellow', 'background' = 'black')
+  ),style = list('color' = '#d4af37', 'background' = "#1C232F", 'text-align' = 'center')
 )
 
 
@@ -517,7 +669,7 @@ app$callback(
                 y = Volume/1000000,
                 color = Name,
                 fill = Name) +
-                geom_smooth() 
+                geom_smooth() + ylab("Volume in millions")
         px1 <- p1 + theme(axis.title.x = element_blank())
         
         return(ggplotly(px1))
@@ -529,9 +681,24 @@ app$callback(
     list(input('OHLC', 'value')), #dccDropdownID
     function(ycol) {
         options(repr.plot.width = 10, repr.plot.height = 300)
-        p2 <- ggplot(price_OHLC, aes(x = New_date1, y = !!sym(ycol), col = Name)) + geom_line()
+        p2 <- ggplot(price_OHLC, aes(x = New_date1, y = !!sym(ycol), col = Name)) + geom_line() + ggthemes::scale_fill_tableau()
         px2 <- p2 + facet_wrap(~Name, scales = "free", ncol = 3) + theme_minimal() + theme(legend.position="none") + ylab("Price (USD)")
         return(ggplotly(px2))
+    }
+)
+
+app$callback(
+    output('hist1', 'figure'), #dccGraphID
+    list(input('OHLC', 'value')), #dccDropdownID
+    function(ycol) {
+        p5 <- ggplot(price) +
+            aes(x = New_date1,
+                y = Volume/1000000,
+                color = Name,
+                fill = Name) +
+                geom_smooth() + ylab("Volume in millions")
+        px5 <- p5 + theme(axis.title.x = element_blank())
+        return(ggplotly(px5))
     }
 )
 
@@ -540,7 +707,7 @@ app$callback(
     list(input('OHLC', 'value')), #dccDropdownID
     function(ycol) {
         options(repr.plot.width = 10, repr.plot.height = 300)
-        p3 <- ggplot(price_rest, aes(Name, RollingAvg7_Close, color=Name)) + geom_point() + geom_boxplot()
+        p3 <- ggplot(price_rest, aes(Name, RollingAvg7_Close, color=Name)) + geom_point() + geom_boxplot() + ggthemes::scale_fill_tableau()
         px3 <- p3 + theme_minimal() + theme(legend.position="none") + ylab("Price (USD)")
         return(ggplotly(px3))
     }
